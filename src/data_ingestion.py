64a95a5e9195f03a7f5b54f636e6cd11e0e776f2
Kamesh Kotwani <kameshkotwani@gmail.com>
2024-09-09 23:14:34 +0100

    added dvc add and added data

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -1,3 +1,10 @@
 import pandas as pd
+from pathlib import Path
+import os
+data = pd.read_csv("https://raw.githubusercontent.com/araj2/customer-database/master/Ecommerce%20Customers.csv")
 
-data = pd.read_csv("https://github.com/araj2/customer-database/blob/master/Ecommerce%20Customers.csv")
+data:pd.DataFrame = data.iloc[:,3:]
+
+data = data[data['Length of Membership'] > 3]
+
+data.to_csv(os.path.join('data/raw','customer.csv'),index=False)
